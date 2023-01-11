04091307388957fd5846d9ff88069e1afea135e5
Mohan-Venkata95733 <122424797+Mohan-Venkata95733@users.noreply.github.com>
2023-01-11 11:01:07 +0530

    Update binary subtraction .c

--- binary subtraction .c
+++ binary subtraction .c
@@ -61,3 +61,34 @@ void Subtract(int n, int a[], int b[])
 			if(a[i] == 1)
 				a[i] = 0;
 			else
+				a[i] = 1;
+		}
+		for(int i = n - 1; i >= 0; i--)
+		{
+			if(a[i] == 0)
+			{
+				a[i] = 1;
+				break;
+			}
+		else
+			a[i] = 0;
+		}
+	
+		printf("-");		
+	
+		for(int i = 0; i < n; i++)
+		{
+			printf("%d",a[i]);
+		}
+	}
+}
+int main() 
+{
+	int n;
+	n = 5;		
+	int a[] = {1, 0, 1, 0, 1},
+		b[] = {0, 0, 1, 1, 1};
+	
+	Subtract(n,a,b);
+	return 0;
+}
